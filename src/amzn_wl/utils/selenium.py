909c57a2041904af0262d5487d95f7b0de2065c6
Taro Sato <okomestudio@gmail.com>
2025-10-17 14:38:19 -0700

    Adjust waiting

--- src/amzn_wl/utils/selenium.py
+++ src/amzn_wl/utils/selenium.py
@@ -1,5 +1,6 @@
 """Selenium utilities."""
 
+import logging
 import time
 
 from selenium import webdriver
@@ -8,19 +9,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+logger = logging.getLogger(__name__)
+
 
 def scroll_till_fully_loaded(
-    driver: webdriver.Chrome, stop_condition: str, max_try: int = 40, wait: int = 1
+    driver: webdriver.Chrome, stop_condition: str, max_try: int = 50, wait: int = 1.5
 ) -> None:
     """Scroll down a wishlist page till the end."""
-    for _ in range(max_try):
+    for trial in range(max_try):
         try:
             WebDriverWait(driver, wait).until(
                 EC.presence_of_element_located((By.ID, stop_condition))
             )
         except exceptions.TimeoutException:
+            logger.info(f"Scrolling to find stop condition... ({trial + 1}/{max_try})")
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight / 2);")
-            time.sleep(1)
+            time.sleep(wait)
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             continue
         else:
